7e9c202151ea4b73bc33488fe8cb6950d0fdaea2
dannymolyneux <dmolyneux@colgate.edu>
2025-01-30 15:15:14 -0500

    Task 2 step 3d
    Still have a backslash escape issue

--- Lab2Script.R
+++ Lab2Script.R
@@ -3,49 +3,52 @@
 library(stringr)
 
 # Step 1
-list.dirs("Musicr")
+list.dirs("Musicr")  #lists all subdirectories of Musicr
 
 # Step 2
 str_count("MUSICR/PeopleStuff", pattern = "/")
 
-sub.directories = c(list.dirs("Musicr"))
-album.sub = c()
+sub.directories = c(list.dirs("Musicr")) #puts subdirectories into a vector
+album.sub = c() #empty vector for the subset of album subdirectories
 for(direc in sub.directories){
-  print(direc)
-  if (str_count(direc, pattern = "/") == 2) {
-    album.sub = append(album.sub, direc)
+  if (str_count(direc, pattern = "/") == 2) {  #if there are two forward slashes in the directory, it is an album
+    album.sub = append(album.sub, direc) #add the album to the new vector
   }
 }
-print(album.sub)
+#print(album.sub)
 
 # Step 3
-code.to.process = c()
-files = c()
-for(sub in album.sub){
-  sub.files = list.files(sub)
-  if(sum(str_count(sub.files, pattern = "wav")) > 0) {
+files = c()  #empty vector for all .wav files
+for(sub in album.sub){ #loop through albums
+  sub.files = list.files(sub) #list of files for each album
+  if(sum(str_count(sub.files, pattern = "wav")) > 0) { #if a .wav file, add it to files vector
     files = append(files, sub.files)
   }
 }
-print(files)
+#print(files)
 
-
-for(album in album.sub){
-  tracks = list.files(album)
-  for(track in tracks){
-    name = str_sub(track, start = 1, end = length(track)-6)
-    location = paste('"', album, track, '"', sep = "/")
-    split = str_split(name, "-")
-    track.name = split[[1]][length(split[[1]])]
-    artist = split[[1]][length(split[[1]])-1]
+code.to.process = c() #code to process vector
+for(album in album.sub) { #loop through albums
+  tracks = list.files(album) #list of tracks for that album
+  for(track in tracks){ #loop through the tracks in this album
+    name = str_sub(track, start = 1, end = length(track)-6) #tracks filename without the .wav
+    location = paste('"', album, track, '"', sep = "/") #album subdirectory/trackname
+    split = str_split(name, "-") #splits the name between the dashes
+    track.name = split[[1]][length(split[[1]])] #takes just the track name
+    artist = split[[1]][length(split[[1]])-1] #artist name for that track
+    album.split = str_split(album, "/")  #splits the album subdirectory between forward slashes
+    just.album = album.split[[1]][length(album.split[[1]])] #takes just the album name
+    output = paste(artist, "-", just.album, "-", track.name, ".json", sep = "") #desired output
+    result = paste('streaming_extractor_music.exe "', track.name, '.wav" "', output, '"', sep = "")
+    code.to.process = append(code.to.process, result)
     #print(name)
     #print(location)
     #print(split)
     #print(track.name)
     #print(artist)
-    album.split = str_split(album, "/")
-    just.album = album.split[[1]][length(album.split[[1]])]
-    output = paste(artist, just.album, track.name, ".json", sep = "-")
-    print(output)
+    #print(output)
+    #print(result)
   }
 }
+#print(album.sub)
+print(code.to.process)
